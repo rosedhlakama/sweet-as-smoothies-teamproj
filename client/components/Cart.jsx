77bdde51661afe0a053f4fb39384addf141f0e54
Chloe <45995939+chloeseun@users.noreply.github.com>
2019-09-10 18:21:16 +1200

    wip update cart

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import CartList from './CartList'
-import { navigate } from '../actions'
+import { navigate, updateQuantities } from '../actions'
 
 class Cart extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cart: props.cart
+    }
+  }
+
   navigateToListing = () => {
     this.props.dispatch(navigate('listing'))
+  }
+
+  updateQuantity = (id, quantity) => {
+    console.log('hey')
+    this.state.cart.map(item => item.id === id ? item.quantity = quantity : item)
   }
 
   render() {
@@ -14,11 +27,11 @@ class Cart extends Component {
       <div className='cart'>
         <p className="welcome">Thirsty? Sweet! You're one step closer to a quenching.</p>
 
-        <CartList cart={this.props.cart} />
+        <CartList cart={this.props.cart} updateQuantity={updateQuantities} />
 
         <p className="actions">
           <button onClick={this.navigateToListing}>Continue shopping</button>
-          <button>Update</button>
+          <button onClick={() => this.props.dispatch(updateQuantities(this.this.state.cart))}>Update</button>
           <button className="button-primary">Checkout</button>
         </p>
       </div>
--- client/components/CartList.jsx
+++ client/components/CartList.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import CartListItem from './CartListItem'
 
-export default function CartList({ cart }) {
+export default function CartList(props) {
+  const { cart } = props
   return (
     <table>
       <thead>
@@ -13,7 +14,7 @@ export default function CartList({ cart }) {
       </thead>
 
       <tbody>
-        {cart.map(item => <CartListItem key={item.id} beer={item} />)}
+        {cart.map(item => <CartListItem key={item.id} beer={item} updateQuantity={props.updateQuantity} />)}
       </tbody>
     </table>
   )
