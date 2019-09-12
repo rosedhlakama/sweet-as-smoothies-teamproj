5c43c11f6c706d2486da05a67573ab510fdda9a6
Alex-Abernethy <Alexander.Abernethy@gmail.com>
2019-09-13 10:18:21 +1200

    sugar to home navigation

--- client/components/Sugar.jsx
+++ client/components/Sugar.jsx
@@ -3,8 +3,13 @@ import { connect } from 'react-redux'
 
 import CartList from './CartList'
 
+import { navigate } from '../actions'
+
 class Sugar extends Component {
-  
+
+  navigateToListing = () => {
+    this.props.dispatch(navigate('listing'))
+  }
 
   render() {
     return (
@@ -12,6 +17,10 @@ class Sugar extends Component {
         <h2>Smoothie Sugar</h2>
         <p>Sugar content per 100mls of smoothie</p>
         <p>Sugar: </p>
+
+        <p className="actions">
+          <button onClick={this.navigateToListing}>Home</button>
+        </p>.
       </div>
     )
   }
