52fa8eca2a5bfd63df6a2f09ef5953ecf30f7afb
WillHoughton <will.houghton2000@gmail.com>
2019-09-13 10:20:59 +1200

    add font colors

--- client/components/Blending.jsx
+++ client/components/Blending.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 const WaitIndicator = (props) => {
-  return props.waiting
-    ? <img className='wait-blending' src='blender.png' />
-    : null
+  return props.waiting ? (
+    <img className='wait-blending' src='../../images/blender.png' />
+  ) : null;
 }
 
 function mapStateToProps (state) {
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-      <h1><span className="fa fa-apple-alt"></span><span className="fa fa-lemon"></span>Sweet As Smoothies <span className="fa fa-lemon"></span><span className="fa fa-apple-alt"></span></h1>
+      <h1><span className="logo-icon"></span><span className="fa fa-lemon"></span>Sweet As Smoothies <span className="fa fa-lemon"></span><span className="fa fa-apple-alt"></span></h1>
+      <p className="welcome">In life much like smoothies you get out what you put in.</p>
     </header>
   )
 }
