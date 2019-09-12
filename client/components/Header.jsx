4e4d791db14a94fa8c495fa5a060365fdcc9e212
rosedhlakama <rosejembe@live.com>
2019-09-12 21:04:04 +1200

    add blender image

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-      <h1><span className="fa fa-beer"></span> Sweet As Beers <span className="fa fa-beer"></span></h1>
+      <h1><span className="fa fa-beer"></span> Sweet As Smoothies <span className="fa fa-beer"></span></h1>
     </header>
   )
 }
