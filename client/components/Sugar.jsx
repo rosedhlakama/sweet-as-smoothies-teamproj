66b9ab7f7f9aba5840033fd16c10730ac7fd7b7f
Chloe <45995939+chloeseun@users.noreply.github.com>
2019-09-13 13:51:36 +1200

    fix missing sugarcontent in cart item state

--- client/components/Sugar.jsx
+++ client/components/Sugar.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import CartList from './CartList'
-
 import { navigate } from '../actions'
 
 class Sugar extends Component {
