81843d2a407f28bce32053b8c5729c7c31455327
Chloe <45995939+chloeseun@users.noreply.github.com>
2019-09-13 15:21:54 +1200

    fix update quantity

--- client/components/CartListItem.jsx
+++ client/components/CartListItem.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { removeFromCart } from '../actions'
 
 function CartListItem(props) {
-  const { beer } = props
+  const { name, quantity, id } = props.fruit
 
   const handleRemove = (id) => {
     props.dispatch(removeFromCart(id))
@@ -12,12 +12,12 @@ function CartListItem(props) {
 
   return (
     <tr>
-      <td>{beer.name}</td>
+      <td>{name}</td>
       <td>
-        <input className="update-input" value={beer.quantity} onChange={(e) => props.updateQuantity(beer.id, e.target.value)} />
+        <input className="update-input" value={quantity} onChange={(e) => props.updateQuantity(id, parseInt(e.target.value))} />
       </td>
       <td>
-        <button className="trash" onClick={() => handleRemove(beer.id)}><span className="fa fa-trash fa-2x"></span></button>
+        <button className="trash" onClick={() => handleRemove(id)}><span className="fa fa-trash fa-2x"></span></button>
       </td>
     </tr>
   )
--- client/components/Sugar.jsx
+++ client/components/Sugar.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import Blending from './Blending'
-
 class Sugar extends Component {
   constructor(props) {
     super(props)
