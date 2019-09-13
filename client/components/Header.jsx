1f2458ed7d3d1db08384e10f163ed34b602b55d4
WillHoughton <will.houghton2000@gmail.com>
2019-09-13 13:02:12 +1200

    add header hover

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {navigate }from '../actions'
+import { connect } from 'react-redux';
 
-export default function Header() {
+class Header extends Component {
+  navigateToListing = () => {
+    this.props.dispatch(navigate('listing'))
+  }
+render(){ 
   return (
-    <header className="header">
-      <h1 className="header-text"> Sweet As Smoothies </h1>
-      <h1 className="tri-apple"> <span className="fa fa-apple-alt apple1"> </span> <span className="fas fa-apple-alt apple2"> </span> <span className="fas fa-apple-alt apple3"> </span>
+    <header onClick={this.navigateToListing} className='header'>
+      <h1 className='header-text'> Sweet As Smoothies </h1>
+      <h1 className='tri-apple'>
+        {' '}
+        <span className='fa fa-apple-alt apple1'> </span>{' '}
+        <span className='fas fa-apple-alt apple2'> </span>{' '}
+        <span className='fas fa-apple-alt apple3'> </span>
       </h1>
-      <p className="welcome">In life much like smoothies you get out what you put in.</p>
+      <p className='welcome'>
+        In life much like smoothies you get out what you put in.
+      </p>
     </header>
-  )
+  );
 }
+}
+
+export default connect()(Header)
